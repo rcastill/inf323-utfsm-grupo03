abf9f6579f363e372bec4bffe9a73edf29410f62
Diego Villegas Arenas <diego.villegas.12@sansano.usm.cl>
2017-12-06 12:09:19 -0300

    Se fortifico la pagin web. Se resolvio el error, creando una carpeta static, y agregando la localizacion de esta en el settings.py

--- grupo03_site/grupo03_site/settings.py
+++ grupo03_site/grupo03_site/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'grupo03_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
